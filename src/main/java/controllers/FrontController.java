ed7999dc4215c193990716b2dedacbbd8d9d330f
hana <38810330+hanataku@users.noreply.github.com>
2023-03-14 16:59:33 +0900

    FrontController作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="FrontController", urlPatterns={"/"})
+import actions.ActionBase;
+import actions.UnknownAction;
+import constants.ForwardConst;
+
+@WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -18,10 +23,44 @@ public class FrontController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //パラメータに該当するActionクラスのインスタンス
+        ActionBase action = getAction(request, response);
+
+        //サーブレットコンテキスト、リクエスト、レスポンスをActionインスタンスのフィールドに設定
+        action.init(getServletContext(), request, response);
+
+        //Actionクラスの処理を呼び出し
+        action.process();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
+
+    /**
+     * リクエストパラメータの値から該当するActionクラスのインスタンスを作成し、返却する
+     * (例:パラメータが action=Employee の場合、actions.EmployeeActionオブジェクト)
+     * @param request リクエスト
+     * @param response レスポンス
+     * @return
+     */
+     private ActionBase getAction(HttpServletRequest request,HttpServletResponse response) {
+         Class type = null;
+         ActionBase action = null;
+
+         try {
+            String actionString = request.getParameter(ForwardConst.ACT.getValue());
+             type = Class.forName(String.format("actions.%sAction", actionString));
+
+             action = (ActionBase)(type.asSubclass(ActionBase.class)
+                         .getDeclaredConstructor()
+                         .newInstance());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | SecurityException | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException e) {
+            action = new UnknownAction();
+        }
+         return action;
+     }
 
 }
